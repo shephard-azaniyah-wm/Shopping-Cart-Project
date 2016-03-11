ef243441fec58e61f482e32f422001902b8d182e
Azaniyah Shephard <session1@START-Coding-MBP-22.local>
2016-03-11 15:18:17 -0700

    no message

--- Workbench connector.php
+++ Workbench connector.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $con = new PDO('mysql:host=127.0.0.1;dbname=Lucky Bunny', 'root', 'root');
+    $con = new PDO('mysql:host=127.0.0.1;dbname=mydb', 'root', 'root');
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
